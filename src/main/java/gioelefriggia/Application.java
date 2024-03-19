2bd03abb3b9fda6708ab0c7a1f80795f01f9f3a3
gioele <gioelefriggia@hotmail.com>
2024-03-19 15:25:36 +0000

    finish 2.0

--- src/main/java/gioelefriggia/Application.java
+++ src/main/java/gioelefriggia/Application.java
@@ -16,16 +16,21 @@ public class Application {
         EventoDAO eventoDao = new EventoDAO(em);
 
         // Esempio di creazione e salvataggio di un evento
-        Evento evento = new Evento("Concerto Blues", new Date(), "Evento di musica blues al teatro", TipoEvento.PUBBLICO, 1500);
-        eventoDao.save(evento);
+        Evento concertoRock = new Evento("Concerto Rock", new Date(), "Evento di musica rock all'aperto", TipoEvento.PUBBLICO, 500);
+        eventoDao.save(concertoRock);
 
+        Evento concertoBlues = new Evento("Concerto Blues", new Date(), "Evento di musica blues al teatro", TipoEvento.PUBBLICO, 1500);
+        eventoDao.save(concertoBlues);
+
+        Evento concertoJazz = new Evento("Concerto Jazz", new Date(), "Evento di musica jazz al centro culturale", TipoEvento.PUBBLICO, 800);
+        eventoDao.save(concertoJazz);
 
         // Esempio di recupero di un evento tramite ID
-        Evento eventoRecuperato = eventoDao.getById(evento.getId());
+        Evento eventoRecuperato = eventoDao.getById(concertoRock.getId());
         System.out.println(eventoRecuperato);
 
         // Esempio di eliminazione di un evento
-        eventoDao.delete(evento.getId());
+        eventoDao.delete(concertoBlues.getId());
 
         em.close();
         emf.close();
